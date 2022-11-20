6ce62046a50a349f4c2d56317202957fd90c87b0
Jackson-Wozniak <jjw30@comcast.net>
2022-11-20 10:25:47 -0500

    show full location names instead of country/continent codes

--- src/components/AirportCard.js
+++ src/components/AirportCard.js
@@ -9,9 +9,8 @@ function AirportCard(props){
             <hr />
             <label>Airport Name <p>{props.airport.name}</p></label>
             <label>ICAO Code <p>{props.airport.icaoCode}</p></label>
-            <label>Coordinates <p>{props.airport.latitude} , {props.airport.longitude}</p></label>
-            <label>Location <p>{props.airport.country},{props.airport.continent}</p></label>
-            <label>Type <p>{props.airport.size}</p></label>
+            <label>Coordinates <p>{props.airport.latitude}, {props.airport.longitude}</p></label>
+            <label>Location <p>{props.airport.country}, {props.airport.continent}</p></label>
         </div>
     );
 }
